f6acaba4600ca3011d42af29fcf3fc7467037659
Chetan Nair <cnair@stanford.edu>
2021-12-31 21:29:14 +0400

    Debug Value Corrected

--- NewsHQ/settings.py
+++ NewsHQ/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_on_heroku
+import django_on_heroku  # type: ignore
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['newshq.herokuapp.com']
 
