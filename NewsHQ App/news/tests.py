e5b05fb17741a876b46208d3f67aed0f00c3de6a
Chetan Nair <cnair@stanford.edu>
2022-01-07 22:58:29 +0400

    Added some unit tests

--- NewsHQ App/news/tests.py
+++ NewsHQ App/news/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Article, Website
 
-# Create your tests here.
+
+class ArticleTestCase(TestCase):
+    def setUp(self):
+        self.website_ = "https:retuers.com"
+        self.website = Website.objects.create(url=self.website_)
+        self.article = Article.objects.create(
+            title="Testing",
+            text="Testing Text",
+            url="https://reuters.com/testing",
+            parent_website=self.website,
+        )
+
+    def test_website_article_fk(self):
+        self.assertEqual(self.article.parent_website, self.website_)
+
+
+class WebsiteTestCase(TestCase):
+    def setUp(self):
+        self.website = Website.objects.create(url="https://reuters.com")
